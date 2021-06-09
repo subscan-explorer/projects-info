b507068e98f3d29f7e115d0a602cd8662b928432
pangwa <pangwa@gmail.com>
2021-06-09 15:20:16 +0800

    add sakura info

--- networks/kusama.js
+++ networks/kusama.js
@@ -1,3 +1,14 @@
+import sakura_icon from "url:../assets/images/sakura.svg";
+
 export default {
-  parachains: []
-}
+  parachains: [
+    {
+      info: "sakuraClover",
+      paraId: 2016,
+      text: "Sakura",
+      project: "Sakura",
+      websiteLink: "https://clover.finance/sakura",
+      logo: sakura_icon,
+    },
+  ],
+};
