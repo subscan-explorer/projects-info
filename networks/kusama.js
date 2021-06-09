2cc788215041766383b441de2947ee6ff0373626
Aki Wu <wuminzhe@gmail.com>
2021-06-09 17:00:32 +0800

    Add blank text as default

--- networks/kusama.js
+++ networks/kusama.js
@@ -10,7 +10,16 @@ export default {
       "Description (zh)": "Crab 网络是 Darwinia 网络的一个有经济价值的金丝雀网络",
       "Parachain Crowdloans Allocation (en)": "https://darwinianetwork.medium.com/darwinia-crabs-kusama-parachain-auction-strategy-3f37cbfdfe4",
       "Parachain Crowdloans Allocation (zh)": "https://mp.weixin.qq.com/s/fvQIiQp0xqkgXY4bG8Mskw",
-      "Website Link": "https://crab.network/"
+      "Auction Reward & Reward Vesting Schedule (en)": "",
+      "Auction Reward & Reward Vesting Schedule (zh)": "",
+      "Financing Information (en)": "",
+      "Financing Information (zh)": "",
+      "Website Link": "https://crab.network/",
+      "Twitter Link": "",
+      "Telegram Link": "",
+      "Discord Link": "",
+      "Medium Link": "",
+      "Github Link": ""
     }
   ]
 }
