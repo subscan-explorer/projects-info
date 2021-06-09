cfc2f81662042461c7170e9dc2c7293b4b489c57
Aki Wu <wuminzhe@gmail.com>
2021-06-09 16:37:33 +0800

    Crab

--- networks/kusama.js
+++ networks/kusama.js
@@ -1,3 +1,16 @@
+import crab_icon from "url:../assets/images/crab.svg";
+
 export default {
-  parachains: []
+  parachains: [
+    {
+      "Para ID": 2006,
+      "Text": "Crab",
+      "Logo": crab_icon,
+      "Description (en)": "Crab is a canary network with real economics for Darwinia",
+      "Description (zh)": "Crab 网络是 Darwinia 网络的一个有经济价值的金丝雀网络",
+      "Parachain Crowdloans Allocation (en)": "https://darwinianetwork.medium.com/darwinia-crabs-kusama-parachain-auction-strategy-3f37cbfdfe4",
+      "Parachain Crowdloans Allocation (zh)": "https://mp.weixin.qq.com/s/fvQIiQp0xqkgXY4bG8Mskw",
+      "Website Link": "https://crab.network/"
+    }
+  ]
 }
