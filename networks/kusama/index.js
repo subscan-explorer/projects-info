dc861cbe788fc04c26ca05778c56c49e61368851
jasl <jasl9187@hotmail.com>
2021-06-10 13:44:35 +0800

    add khala

--- networks/kusama/index.js
+++ networks/kusama/index.js
@@ -1,7 +1,8 @@
 export default {
     parachains: {
         "2016": "sakura.json",
-        "2012": "crust-shadow.json" ,
-        "2009": "polkasmith.json"
+        "2012": "crust-shadow.json",
+        "2009": "polkasmith.json",
+        "2004": "khala.json"
     }
 }
