1dd3045970f2f52b99091dfcbd2047bb9a71c7aa
Alex D <pr0fedt@gmail.com>
2021-09-03 11:25:43 +0300

    added core information; WIP

--- networks/kusama/index.js
+++ networks/kusama/index.js
@@ -5,6 +5,7 @@ export default {
         "2016": "sakura.json",
         "2012": "crust-shadow.json",
         "2009": "polkasmith.json",
-        "2004": "khala.json"
+        "2004": "khala.json",
+        "2089": "genshiro.json"
     }
 }
