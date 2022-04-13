275e33e3fe98ef8704d38e47606e2ec48cd665a7
vzxh <keqiao.wu@itering.com>
2022-04-13 17:28:22 +0800

    include parachain images in npm package

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ kusamaJsons.keys().forEach((k) => {
   const c = kusamaJsons(k);
   if (c.data && c.data.ParaID) {
     kusamaParachains[c.data.ParaID] = c.data;
+    try {
+      const logoPaths = c.data.Logo.split("/");
+      const imageName = logoPaths[logoPaths.length - 1];
+      kusamaParachains[
+        c.data.ParaID
+      ].logo = require(`./assets/images/${imageName}`);
+    } catch {}
   }
 });
 
@@ -13,6 +20,13 @@ polkadotJsons.keys().forEach((k) => {
   const c = polkadotJsons(k);
   if (c.data && c.data.ParaID) {
     polkadotParachains[c.data.ParaID] = c.data;
+    try {
+      const logoPaths = c.data.Logo.split("/");
+      const imageName = logoPaths[logoPaths.length - 1];
+      polkadotParachains[
+        c.data.ParaID
+      ].logo = require(`./assets/images/${imageName}`);
+    } catch {}
   }
 });
 
@@ -22,6 +36,13 @@ rococoJsons.keys().forEach((k) => {
   const c = rococoJsons(k);
   if (c.data && c.data.ParaID) {
     rococoParachains[c.data.ParaID] = c.data;
+    try {
+      const logoPaths = c.data.Logo.split("/");
+      const imageName = logoPaths[logoPaths.length - 1];
+      rococoParachains[
+        c.data.ParaID
+      ].logo = require(`./assets/images/${imageName}`);
+    } catch {}
   }
 });
 
@@ -31,6 +52,13 @@ westendJsons.keys().forEach((k) => {
   const c = westendJsons(k);
   if (c.data && c.data.ParaID) {
     westendParachains[c.data.ParaID] = c.data;
+    try {
+      const logoPaths = c.data.Logo.split("/");
+      const imageName = logoPaths[logoPaths.length - 1];
+      westendParachains[
+        c.data.ParaID
+      ].logo = require(`./assets/images/${imageName}`);
+    } catch {}
   }
 });
 
